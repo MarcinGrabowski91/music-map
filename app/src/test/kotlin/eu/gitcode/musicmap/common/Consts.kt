d5278edaa4eb3a5be2f102db624977009ca44248
marcin.grabowski <marcingrabowski91@gmail.com>
2018-08-13 10:37:28 +0200

    Tests added

---
+++ app/src/test/kotlin/eu/gitcode/musicmap/common/Consts.kt
@@ -0,0 +1,13 @@
+package eu.gitcode.musicmap.common
+
+class Consts {
+    companion object {
+        val EXAMPLE_STRING: String = "Example string"
+        val EXAMPLE_STRING_2: String = "Example string 2"
+        val EXAMPLE_LONG: Long = 1
+        val EXAMPLE_INT: Int = 1
+        val EXAMPLE_FLOAT: Float = 1f
+        val EXAMPLE_BOOLEAN: Boolean = false
+        val EXAMPLE_DOUBLE: Double = 1.0
+    }
+}
